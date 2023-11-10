b9f898dc0a8e8e5b8d8a8db464329ffbd4feb9ce
Robert G <robert.gajewski@gmail.com>
2023-11-10 19:56:54 +0100

    Add SVG icon to menu_page in the index.php file; add handleForm function with input:hidden HTML-fields  Changes to submission: new file: wp-content/plugins/our-word-filter-plugin/custom.svg chenged: wp-content/plugins/our-word-filter-plugin/index.php new file: wp-content/plugins/our-word-filter-plugin/styles.css

---
+++ wp-content/plugins/our-word-filter-plugin/index.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+* Plugin Name: Our Word filter Plugin
+* Description: Replaces a list of word.
+* Verstion: 1.0
+* Author: ga1robe
+* Author URI: http://kreatywne-media.pl/graphics-training/
+*/
+
+if(! defined('ABSPATH')) exit(); /* Exit if accessed directly */
+
+class OurWordFilterPlugin {
+    function __construct(){
+        add_action('admin_menu', array($this, 'ourMenu'));
+    }
+
+    function ourMenu(){
+        // add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'our-word-filter', array($this, 'wordFilterPage'), 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMCAyMEMxNS41MjI5IDIwIDIwIDE1LjUyMjkgMjAgMTBDMjAgNC40NzcxNCAxNS41MjI5IDAgMTAgMEM0LjQ3NzE0IDAgMCA0LjQ3NzE0IDAgMTBDMCAxNS41MjI5IDQuNDc3MTQgMjAgMTAgMjBaTTExLjk5IDcuNDQ2NjZMMTAuMDc4MSAxLjU2MjVMOC4xNjYyNiA3LjQ0NjY2SDEuOTc5MjhMNi45ODQ2NSAxMS4wODMzTDUuMDcyNzUgMTYuOTY3NEwxMC4wNzgxIDEzLjMzMDhMMTUuMDgzNSAxNi45Njc0TDEzLjE3MTYgMTEuMDgzM0wxOC4xNzcgNy40NDY2NkgxMS45OVoiIGZpbGw9IiNGRkRGOEQiLz4KPC9zdmc+', 69);
+        $mainPageHook = add_menu_page('Words To Filter', 'Word Filter', 'manage_options', 'our-word-filter', array($this, 'wordFilterPage'), plugin_dir_url(__FILE__).'custom.svg', 54);
+        add_submenu_page('our-word-filter', 'Words To Filter', 'Words List', 'manage_options', 'our-word-filter', array($this, 'wordFilterPage'), null);
+        add_submenu_page('our-word-filter', 'Word Filter Options', 'Options', 'manage_options', 'word-filter-options', array($this, 'optionsSubPage'), null);
+        add_action("load-{$mainPageHook}", array($this, 'mainPageAssets'));
+    }
+
+    function wordFilterPage(){ ?>
+        <div class="wrap">
+            <h1>Word Filter</h1>
+            <?php if(array_key_exists("justsubmitted", $_POST) && $_POST["justsubmitted"] == "true") $this->handleForm() ?>
+            <form method="POST">
+                <input type="hidden" name="justsubmitted" value="true">
+                <?php wp_nonce_field('saveFilterWords', 'ourNonce') ?>
+                <label for="plugin_words_to_filter"><p>Enter a <strong>comma separated</strong> list of words to filter from your site's content.</p></label>
+                <div class="word-filter__flex-container"><textarea name="plugin_words_to_filter" id="plugin_words_to_filter" placeholder="bad, mean, awful, horrible"><?php echo esc_textarea(get_option('plugin_words_to_filter')) ?></textarea></div>
+                <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+            </form>
+        </div>
+    <?php
+    }
+    
+    function optionsSubPage(){ ?>
+        <p>Options</p>
+    <?php
+    }
+
+    function handleForm(){
+        if(array_key_exists('ourNonce', $_POST) AND wp_verify_nonce($_POST['ourNonce'], 'saveFilterWords') AND current_user_can("manage_options")){
+            update_option('plugin_words_to_filter', sanitize_text_field($_POST['plugin_words_to_filter'])); ?>
+            <div class="updated"><p>Your filter words were saved.</p></div>
+        <?php
+        } else { ?>
+            <div class="error"><p>Sorry, you do not have permission to perform that action.</p></div>
+        <?php
+        }
+    }
+
+    function mainPageAssets(){ wp_enqueue_style('filter-admin-css', plugin_dir_url(__FILE__).'styles.css', array()); }
+
+}
+
+$ourWordFilterPlugin = new OurWordFilterPlugin();
+
+?>
