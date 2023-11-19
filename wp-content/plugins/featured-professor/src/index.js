d55c1904cef9fcb4672e2720b353812f75235ea0
Robert G <robert.gajewski@gmail.com>
2023-11-19 22:07:33 +0100

    Użycie 'useSelect' do selekcji postów typu 'professor'; dodanie map 'allFrofs' do opcji w 'select'  Zmiany do złożenia: 	zmieniono:       wp-content/plugins/featured-professor/featured-professor.php 	nowy plik:       wp-content/plugins/featured-professor/package-lock.json 	nowy plik:       wp-content/plugins/featured-professor/package.json 	zmieniono:       wp-content/plugins/featured-professor/src/index.js

--- wp-content/plugins/featured-professor/src/index.js
+++ wp-content/plugins/featured-professor/src/index.js
@@ -1,25 +1,31 @@
 import "./index.scss"
+import {useSelect} from "@wordpress/data"
 
 wp.blocks.registerBlockType("ourplugin/featured-professor", {
-  title: "Professor Callout",
-  description: "Include a short description and link to a professor of your choice",
-  icon: "welcome-learn-more",
-  category: "common",
-  edit: EditComponent,
-  save: function () {
-    return null
-  }
+    title: "Professor Callout",
+    description: "Include a short description and link to a professor of your choice",
+    icon: "welcome-learn-more",
+    category: "common",
+    attributes: { profId: {type: "string"} },
+    edit: EditComponent,
+    save: function () {
+        return null
+    }
 })
 
-function EditComponent() {
-  return (
-    <div className="featured-professor-wrapper">
-      <div className="professor-select-container">
-        We will have a select dropdown form element here.
-      </div>
-      <div>
-        The HTML preview of the selected professor will appear here.
-      </div>
-    </div>
-  )
+function EditComponent(props) {
+    const allProfs = useSelect(select => { return select("core").getEntityRecords("postType", "professor", {"per_page": -1})})
+    console.info(allProfs)
+    if (allProfs == undefined) return <p>Loading...</p>
+    return (
+        <div className="featured-professor-wrapper">
+            <div className="professor-select-container">
+                <select onChange={e => props.setAttributes({profId: e.target.value})}>
+                    <option value="" selected="0">Select a professor</option>
+                    {allProfs.map(prof => { return( <option value={prof.id} selected={props.attributes.profId == prof.id}>{prof.title.rendered}</option> ) })}
+                </select>
+            </div>
+            <div>The HTML preview of the selected professor will appear here.</div>
+        </div>
+    )
 }
