d5528e9e5bb0a7e2a64d8b4eeea06c681eba184c
Robert G <robert.gajewski@gmail.com>
2023-11-20 19:55:15 +0100

    Working with text from professor post type;  Zmiany do złożenia: 	zmieniono:       wp-content/plugins/featured-professor/featured-professor.php 	nowy plik:       wp-content/plugins/featured-professor/inc/generateProfessorHTML.php 	zmieniono:       wp-content/plugins/featured-professor/src/index.js

--- wp-content/plugins/featured-professor/featured-professor.php
+++ wp-content/plugins/featured-professor/featured-professor.php
@@ -9,9 +9,13 @@
 
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
+require_once plugin_dir_path(__FILE__).'inc/generateProfessorHTML.php';
 class FeaturedProfessor {
-  function __construct() { add_action('init', [$this, 'onInit']); }
-  function onInit() {
+    function __construct() {
+        add_action('init', [$this, 'onInit']);
+        add_action('rest_api_init', [$this, 'profHTML']);
+    }
+    function onInit() {
         wp_register_script('featuredProfessorScript', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-i18n', 'wp-editor'));
         wp_register_style('featuredProfessorStyle', plugin_dir_url(__FILE__) . 'build/index.css');
         register_block_type('ourplugin/featured-professor', array(
@@ -19,8 +23,19 @@ class FeaturedProfessor {
             'editor_script' => 'featuredProfessorScript',
             'editor_style' => 'featuredProfessorStyle'
         ));
-  }
-  function renderCallback($attributes) { return '<p>We will replace this content soon.</p>'; }
+    }
+    function renderCallback($attributes) {
+        if($attributes['profId']){
+            wp_enqueue_style('featuredProfessorStyle');
+            return generateProfessorHTML($attributes['profId']);
+        } else { return null; }
+    }
+
+    function profHTML() {
+        register_rest_route('featuredProfessor/v1', 'getHTML', array( 'methods' => WP_REST_SERVER::READABLE, 'callback' => [$this, 'getProfHTML'] ));
+    }
+
+  function getProfHTML($data) { return generateProfessorHTML($data['profId']); }
 
 }
 
